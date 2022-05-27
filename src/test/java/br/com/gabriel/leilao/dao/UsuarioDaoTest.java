ebfd4cc3e80485948c7228fee9cf02d4a7a845ab
gabrieltherock <gabrieldarocha2013@hotmail.com>
2022-05-27 13:15:36 -0300

    criando mais um cenário e refatorando testes

--- src/test/java/br/com/gabriel/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/gabriel/leilao/dao/UsuarioDaoTest.java
@@ -2,27 +2,48 @@ package br.com.gabriel.leilao.dao;
 
 import br.com.gabriel.leilao.model.Usuario;
 import br.com.gabriel.leilao.util.JPAUtil;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import javax.persistence.NoResultException;
 
 class UsuarioDaoTest {
 
     private UsuarioDao dao;
+    private EntityManager em;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.em = JPAUtil.getEntityManager();
+        this.dao = new UsuarioDao(em);
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        em.getTransaction().rollback();
+    }
 
     @Test
-    void testeBuscaUsuarioPeloUsername() {
-        EntityManager em = JPAUtil.getEntityManager();
-        this.dao = new UsuarioDao(em);
-
-        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
-        em.getTransaction().begin();
-        em.persist(usuario);
-        em.getTransaction().commit();
-
+    void deveriaEncontrarUsuarioCadastrado() {
+        Usuario usuario = criarUsuario();
         Usuario encontrado = this.dao.buscarPorUsername(usuario.getNome());
-        assertNotNull(encontrado);
+        Assertions.assertNotNull(encontrado);
+    }
+
+    @Test
+    void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+        criarUsuario();
+        Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("beltrano"));
+    }
+
+    private Usuario criarUsuario() {
+        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
+        em.persist(usuario);
+        return usuario;
     }
 }
